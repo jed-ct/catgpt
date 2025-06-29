48c1f3afcf9e82593a5bdbcdf648dbfcaabf10dd
dedge-ct <tuquerojedric@gmail.com>
2025-06-29 14:21:30 +0800

    finished website

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const btnSubmit = document.getElementById("submit-response");
 const catResponse = document.getElementById("cat-response");
+const catResponseDiv = document.getElementById("response");
+const catPrompt = document.getElementById("cat-prompt")
 const catFace = document.getElementById("cat-face");
 btnSubmit.addEventListener("click", ()=> {
     catResponse.textContent = generateRandomMeows();
+    catPrompt.value = "";
     generateRandomImage();
+    catResponseDiv.style.display = "flex";
 })
 
 function generateRandomMeows() {
-    let maxMeows = 25;
+    let maxMeows = 10;
     let catResponse = "";
     let meowNumber = Math.floor(Math.random() * maxMeows + 1);
     for (let i = 0; i < meowNumber; i++) {
@@ -17,6 +21,6 @@ function generateRandomMeows() {
 }
 
 function generateRandomImage() {
-    let images = ["angry.jpg", "happy.jpg", "sad.jpeg", "serious.jpg", "oia-uia.gif"]
+    let images = ["angry.jpg", "happy.jpg", "sad.jpeg", "serious.jpg", "oia-uia.gif", "dog.jpg", "ermactually.png", "eyebrow.png", "gigachad.jpeg", "jwu.png", "nooo.png", "sad2.jpg", "serious2.jpeg"];
     catFace.src = `./img/${images[Math.floor(Math.random() * images.length)]}`;
 }
