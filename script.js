712faa040925aa5f61cae4b8ee2da58d9935ec47
dedge-ct <tuquerojedric@gmail.com>
2025-06-29 16:06:04 +0800

    Added dark and light theme

--- script.js
+++ script.js
@@ -3,11 +3,45 @@ const catResponse = document.getElementById("cat-response");
 const catResponseDiv = document.getElementById("response");
 const catPrompt = document.getElementById("cat-prompt")
 const catFace = document.getElementById("cat-face");
+const catLogo = document.getElementById("logo");
+const btnChangeTheme = document.getElementById("change-theme");
+const themeLogo = document.getElementById("change-theme-logo");
+const root = document.documentElement;
+let currentTheme = "";
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (darkThemeMq.matches) {
+    currentTheme = "dark";
+    catLogo.src = "./icons/logo-dark.svg";
+    themeLogo.src = "./icons/dark.svg";
+} else {
+    currentTheme = "light";
+    catLogo.src = "./icons/logo.svg";
+    themeLogo.src = "./icons/light.svg";
+}
+
+root.className = currentTheme;
+
 btnSubmit.addEventListener("click", ()=> {
     catResponse.textContent = generateRandomMeows();
     catPrompt.value = "";
     generateRandomImage();
     catResponseDiv.style.display = "flex";
+})
+
+btnChangeTheme.addEventListener("click", ()=> {
+    if (currentTheme == "light") {
+        themeLogo.src = "./icons/dark.svg";
+        catLogo.src = "./icons/logo-dark.svg";
+        currentTheme = "dark";
+    }
+    else {
+        themeLogo.src = "./icons/light.svg";
+        catLogo.src = "./icons/logo.svg";
+        currentTheme = "light";        
+    }
+    root.className = currentTheme;
+
 })
 
 function generateRandomMeows() {
